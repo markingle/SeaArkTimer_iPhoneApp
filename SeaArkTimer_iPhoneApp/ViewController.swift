4662c364c1d7a8665fb26393f8b9e2418baa6c0e
Mark Ingle <mark.ingle@comporium.com>
2019-11-07 20:21:14 -0500

    Initial Commit

---
+++ SeaArkTimer_iPhoneApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeaArkTimer_iPhoneApp
+//
+//  Created by Mark Brady Ingle on 11/7/19.
+//  Copyright © 2019 Mark Brady Ingle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
